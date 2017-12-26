9d5ca167d64799b052c7563e22d9d4e37678e4f5
zhongyanlin <yanlinzhong@126.com>
2017-12-26 11:46:05 +0800

    企智网框架

---
+++ src/pages/home/home.vue
@@ -0,0 +1,46 @@
+<template>
+    <!--title为当前页标题
+        组件要小，如遇list，只将item做成组件，其他的都写在页面中
+    -->
+    <div id="home" v-set-title="title">
+        企智网
+    </div>
+</template>
+
+<script>
+    import {appService} from '../../service/appService'
+    export default {
+        data () {
+            return {
+
+            }
+        },
+        computed:{
+
+        },
+        created: function() {
+            /*基本规则
+            * 所有不需要token的请求都放在这里
+            * 这里不出现window，document等DOM元素
+            * 这里获得的数据都要存储在store中
+            * */
+        },
+        mounted: function() {
+            /*
+            * 所有需要token的请求都放在这里
+            * 可以使用DOM元素
+            * 这里的数据可以放在data中
+            * */
+
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+<style lang="less">
+    /*基本的rem设置都放在base中，不写多个*/
+    @import url('../../assets/css/base.less');
+</style>
+
---
+++ src/router.js
@@ -0,0 +1,88 @@
+/**
+ * Created by zhong on 2017/10/24.
+ *
+ * (resolve) =>{require(['../components/test.vue'],resolve)}
+ *
+ */
+import Vue from 'vue'
+import store from './vuex'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+// 定义全局组件
+import zmodal from './component/common/modal.vue'
+import zbanner from './component/common/banner.vue'
+import zfooter from './component/common/footer.vue'
+import zhistory from './component/common/goback.vue'
+import backhome from './component/common/back-home.vue'
+
+Vue.component('z-modal', zmodal)
+Vue.component('z-banner', zbanner)
+Vue.component('z-footer', zfooter)
+Vue.component('z-history', zhistory)
+Vue.component('z-home', backhome)
+
+
+// 定义组件
+const short = (resolve) => {require(['./pages/short.vue'],resolve)}
+const home = (resolve) => {require(['./pages/home/home.vue'],resolve)}
+// 404错误页面
+const error = (resolve) => {require(['./pages/error.vue'],resolve)}
+
+// 创建路由器实例，配置路由规则
+const routers = [
+    {path: '/d/:shortId', component: short},
+    {name: 'home',path:'/',component: home, meta: {keepAlive: true}},
+    {name: 'err',path:'/s/404',component: error}, //错误跳到404页面
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: __dirname,
+    routes:routers
+})
+
+let isClient = typeof window != 'undefined' ? true : false
+router.beforeEach((to, from, next) => {
+    if(from.name == 'home' || from.name == 'activityList' || from.name == 'activity' || from.name == 'article' || from.name == 'columnlist' || from.name == 'knowledge' || from.name == 'articleList' || from.name == 'live'){
+        if(!isClient) return
+        store.state.height[from.name] = document.body.scrollTop
+        // console.log(document.body.scrollTop,111);
+    }
+    next()
+})
+router.afterEach((to, from, next) => {
+
+    function checkUrl() {
+        if(to.name=='login' || to.name=='password' || to.name=='find' || to.name=='bind' || to.name=='register') return false
+        else if(to.name=='wxbaselogin' || to.name=='wxlogin' || to.name=='wblogin' || to.name=='qqlogin' || to.name=='wxbaselogin') return false
+        else if(to.name=='weChatAct' || to.name=='weChatAdmin' || to.name=='wxBind' || to.name=='wxcode' || to.name=='payOk') return false
+        else if(to.name=='pay' || to.name=='pay2' || to.name=='applyAdmin') return false
+        else{ return true}
+    }
+    if( checkUrl()) {
+        let i = store.state.fromPath.length-1
+        // console.log('from',store.state.fromPath[i],'to',to.path);
+        if(store.state.fromPath[i] != to.path){
+            if(to.name != 'home'){
+                if (i<2 || store.state.fromPath[i - 1] != to.path){
+                    // console.log('记录frompath');
+                    store.state.fromPath.push(to.path);
+                }
+            }else if(store.state.fromPath[i] !='home'){
+                store.state.fromPath.push('home');
+            }
+        }
+
+        if (i<1 || store.state.fromPath[i - 1] != to.path) {
+            // console.log('增加state');
+            if(!isClient) return
+            window.history.pushState({}, '', window.location.href)
+        } else if (store.state.fromPath[i - 1] == to.path) {
+            // console.log('删除frompath',store.state.fromPath);
+            store.state.fromPath.pop();
+        }
+    }
+});
+
+export default router
