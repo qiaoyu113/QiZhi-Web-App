bebde412259f47d83ef40f87450f6069dc975442
guanhongyu <hongyu_guan@163.com>
2017-12-27 15:06:04 +0800

    企智首页

---
+++ src/pages/home/index.vue
@@ -0,0 +1,67 @@
+<template>
+
+    <div class="indexbox">
+        企智网
+    </div>
+</template>
+
+<script>
+    import {appService} from '../../service/appService'
+    export default {
+        data () {
+            return {
+
+            }
+        },
+        computed:{
+
+        },
+        syncData({store}) {
+            /*基本规则
+            * 所有不需要token的请求都放在这里
+            * 这里不出现window，document等DOM元素
+            * 这里获得的数据都要存储在store中
+            * 写法如下
+            * */
+            const that = this;
+            /*
+            * 将所有的请求处理以数组放在promise中
+            * that.data().data调用数据
+            * */
+            return Promise.all([
+                appervice.getParam().then(res=>{
+//                    store.state.homeStore.listImg = res.data;
+                }),
+                service.getParam().then(res=>{
+//                    store.state.homeStore.noticelist = res.data.datas;
+                }),
+            ])
+        },
+        computed: {
+            //将存在store中的数据取出
+            listImg() {
+                return this.$store.state.homeStore.listImg || []
+            },
+            noticelist() {
+                return this.$store.state.homeStore.noticelist || []
+            },
+        },
+        mounted: function() {
+            /*
+            * 所有需要token的请求都放在这里
+            * 可以使用DOM元素
+            * 这里的数据可以放在data中
+            * */
+
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+<style lang="less">
+    /*rem等基本设置都放在base中，不写多个*/
+    @import url('../../assets/css/base.less');
+</style>
+
